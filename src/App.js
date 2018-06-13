68d75bd8b880bd8d93f56c1f05fe172d9eb94579
s-kris <skris4291@gmail.com>
2018-06-12 23:55:20 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { View, Text } from 'react-primitives';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <View>
+        <Text>Rey Music</Text>
+      </View>
     );
   }
 }
