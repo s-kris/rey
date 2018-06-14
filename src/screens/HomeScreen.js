b36ab460f974b34bf85eb3d53051ca0bf7b90c14
s-kris <skris4291@gmail.com>
2018-06-14 18:03:16 -0400

    added tabs

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-primitives';
+import { View, Text, StyleSheet } from 'react-primitives';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 import Player from '../components/Player';
 import bg1 from './../assets/images/bg1.jpg';
 import { borderRadiusLarge } from '../config/Constants';
+import './../styles/react-tabs.css';
 
 const styles = StyleSheet.create({
   rootContainer: {
@@ -23,10 +25,18 @@ const styles = StyleSheet.create({
     width: '75%',
     height: '75%',
     borderRadius: borderRadiusLarge,
+    display: 'flex',
+    flexDirection: 'row',
   },
   playerContainer: {
     width: '30%',
     height: '100%',
+  },
+  tabsContainer: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
 });
 
@@ -37,6 +47,27 @@ class HomeScreen extends React.Component {
         <View style={styles.modulesContainer}>
           <View style={styles.playerContainer}>
             <Player />
+          </View>
+          <View style={styles.tabsContainer}>
+            <Tabs defaultIndex={1}>
+              <TabList>
+                <Tab>Related</Tab>
+                <Tab>Search</Tab>
+                <Tab>Popular</Tab>
+                <Tab>Favourites</Tab>
+                <Tab>Playlists</Tab>
+                <Tab>Profile</Tab>
+              </TabList>
+              <TabPanel>
+                <Text>hello tabs 1</Text>
+              </TabPanel>
+              <TabPanel>
+                <Text>hello tabs 2</Text>
+              </TabPanel>
+              <TabPanel>
+                <Text>hello tabs 3</Text>
+              </TabPanel>
+            </Tabs>
           </View>
         </View>
       </View>
