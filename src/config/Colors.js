4fccf83dd6dafb48bcc6ba8142a644db52252c44
s-kris <skris4291@gmail.com>
2018-06-22 13:07:51 -0400

    MC: Theme Colors are finalised

--- src/config/Colors.js
+++ src/config/Colors.js
@@ -1,7 +1,7 @@
-export const accentColor = '#44bd32';
-export const primaryColor = '#2f3640';
-export const primaryColorLight = '#3B4350';
-export const primaryColorMid = '#353b48';
+export const accentColor = '#6ab04c';
+export const primaryColor = '#2d3436';
+export const primaryColorMid = '#343C3E';
+export const primaryColorLight = '#353D3E';
 
 // not using this, switched to randomcolor js
 export const colorPalette = ['#b71c1c', '#FFAB00', '#673AB7', '#2196F3', '#4caf50', '#35444b'];
