9969b12e81c54579a2df724426f8991c1e6f43c3
s-kris <skris4291@gmail.com>
2018-06-27 00:12:16 -0400

    mc: removed flatlist in modal editplaylist because of some react native exception

--- src/components/EditPlaylist.js
+++ src/components/EditPlaylist.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, FlatList, Button } from 'react-native-web';
+import { View, Text, Button } from 'react-native-web';
 import dayjs from 'dayjs';
+// import shortid from 'shortid';
 
 import { accentColor, primaryColor } from '../config/Colors';
 import EditPlaylistItem from './listItems/EditPlaylistItem';
@@ -67,13 +68,16 @@ class EditPlaylist extends React.Component {
           </Text>
         </View>
         <View style={styles.songsContainer}>
-          <FlatList
-            // keyExtractor={() => shortid.generate()}
+          {/* <FlatList
+            keyExtractor={() => shortid.generate()}
             data={this.state.data}
             renderItem={({ item, index }) => (
               <EditPlaylistItem name={item.label} onClickDelete={() => this._deleteSong(index)} />
             )}
-          />
+          /> */}
+          {this.state.data.map((item, index) => (
+            <EditPlaylistItem key={item.key} name={item.label} onClickDelete={() => this._deleteSong(index)} />
+          ))}
         </View>
         <View style={styles.buttonContainer}>
           <Button title="Save Playlist" color={accentColor} onPress={() => this._onClickSave()} />
