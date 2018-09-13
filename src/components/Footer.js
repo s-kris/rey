d06ff9b0a76ea71ae68cdb76d17760fcbe240300
s-kris <skris4291@gmail.com>
2018-09-12 22:01:25 -0500

    added github link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,6 +14,10 @@ const styles = {
     color: '#FFF',
     cursor: 'pointer',
   },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
 };
 
 class Footer extends React.Component {
@@ -28,23 +32,37 @@ class Footer extends React.Component {
 
   render() {
     return (
-      <View
-        style={styles.rootContainer}
-        onClick={() => {
-          window.open('https://twitter.com/_skris', '_blank');
-        }}
-      >
-        <Text style={styles.attributionText}>
-          Developed by _skris{' '}
-          {/* <a
+      <View style={styles.rootContainer}>
+        <View
+          style={styles.row}
+          onClick={() => {
+            window.open('https://twitter.com/_skris', '_blank');
+          }}
+        >
+          <Text style={styles.attributionText}>
+            Developed by _skris{'  '}
+            {/* <a
             href="https://twitter.com/_skris?ref_src=twsrc%5Etfw"
             className="twitter-follow-button"
             data-show-count="true"
           >
             @_skris
           </a> */}
-        </Text>
-        <img src={TwitterIcon} style={{ cursor: 'pointer', width: 24, height: 24 }} alt="_skris" />
+          </Text>
+          <img src={TwitterIcon} style={{ cursor: 'pointer', width: 24, height: 24 }} alt="_skris" />
+        </View>
+        <View
+          style={styles.row}
+          onClick={() => {
+            window.open('https://github.com/s-kris/rey', '_blank');
+          }}
+        >
+          <Text style={styles.attributionText}>
+            {'  '}
+            |{'  '}Find on Github{'  '}
+          </Text>
+          <img src="https://cdn.iconscout.com/icon/free/png-256/github-146-569237.png" style={{ cursor: 'pointer', width: 24, height: 24 }} alt="rey" />
+        </View>
       </View>
     );
   }
