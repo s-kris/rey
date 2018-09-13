d0b0b685f376c487838127289e03500db6026619
s-kris <skris4291@gmail.com>
2018-09-12 20:50:36 -0500

    .gitignore cleanup

--- src/config/firebase.js
+++
@@ -1,8 +0,0 @@
-export const fbaseConfig = {
-  apiKey: 'AIzaSyCFa1cvEKwfEd4sNIF9i_3R4jMdwp7oRT0', // replace with new apikey
-  authDomain: 'reymusic-co.firebaseapp.com',
-  databaseURL: 'https://reymusic-co.firebaseio.com',
-  projectId: 'reymusic-co',
-  storageBucket: 'reymusic-co.appspot.com',
-  messagingSenderId: '150115433530',
-};
